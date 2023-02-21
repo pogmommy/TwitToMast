efb9e15022bcf515357d2b1b7e05873275074117
Penelope Gwen / Pogmommy <support@pogmom.me>
2023-02-21 10:50:48 -0700

    Add files via upload

---
+++ macOS_loop.sh
@@ -0,0 +1,12 @@
+
+echo 'export PATH=$PATH:/usr/local/bin/chromedriver' >> ~/.zshenv
+source ~/.zshenv
+if [ `wc -l ./output/output.txt | awk '{print $1}'` -ge "500" ]
+then
+	mv ./output/output.txt "./output/$(date +"%m-%d-%y-%h-%m-%s").txt"
+fi
+echo 'running script...' >> ./output/output.txt
+/bin/date >> ./output/output.txt
+/usr/local/bin/node ./TwitToMast.js -tqp -u pogmommy -n 10 >> ./output/output.txt
+/usr/bin/pkill Chrome
+/usr/bin/killall node
